940e5e5c8d34c07b6185baf134a0a22b577ae1da
Loren Cipriano <loren25cipriano@gmail.com>
2021-02-25 15:46:24 -0800

    added login erb and login contorller in skaterscontroller

--- app/controllers/skaters_controller.rb
+++ app/controllers/skaters_controller.rb
@@ -5,18 +5,23 @@ class SkatersController < ApplicationController
         erb :'skaters/signup'
     end 
 
-    # post '/signup' do
-    #     skater = Skater.new(params)
-    #     # validate our author object
-    #     # if author.username != ""
-    #     if skater.username.blank? || skater.password.blank? || Skater.find_by_username(params[:username])
-    #         redirect '/signup'
-    #     else 
-    #         author.save
-    #         session[:skater_id] = skater.id # logging user in
-    #        # redirect '/posts'
-    #     end 
-    # end
+    post '/signup' do
+        skater = Skater.new(params)
+        # validate our author object
+        # if author.username != ""
+        if skater.username.blank? || skater.password.blank? || Skater.find_by_username(params[:username])
+            redirect '/signup'
+        else 
+            skater.save
+            session[:skater_id] = skater.id # logging user in
+           redirect '/posts'
+        end 
+    end
+
+
+    get '/login' do # render the login form
+        erb :"skaters/login"
+    end 
 
 
 end 
