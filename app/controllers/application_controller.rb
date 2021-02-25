1cb2896f8844d59946b434ed59923ba964960340
Loren Cipriano <loren25cipriano@gmail.com>
2021-02-25 15:55:45 -0800

    added helpers in applicatioincontroller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,12 +6,26 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
     enable :sessions
     set :session_secret, "password_security"
-  
+    register Sinatra::Flash
 
   end
 
   get "/" do
     erb :welcome
   end
+
+  helpers do # makes these methods availble to controller and views
+
+    # return the logged in user
+   def current_user # return logged in user 
+    @current_user ||= Skater.find_by_id(session[:skater_id]) #memoization
+   end 
+
+    # check if a user logged in
+    def logged_in?
+      !!session[:skater_id]
+    end 
+
+  end 
 
 end
