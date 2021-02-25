6ba25155bce5e183771984c8a8876eb1f9a6bdbe
Loren Cipriano <loren25cipriano@gmail.com>
2021-02-25 11:53:48 -0800

    created filled out signup.erb

---
+++ app/controllers/skaters_controller.rb
@@ -0,0 +1,22 @@
+class SkatersController < ApplicationController
+
+
+    get '/signup' do # render signup form
+        erb :'skaters/signup'
+    end 
+
+    # post '/signup' do
+    #     skater = Skater.new(params)
+    #     # validate our author object
+    #     # if author.username != ""
+    #     if skater.username.blank? || skater.password.blank? || Skater.find_by_username(params[:username])
+    #         redirect '/signup'
+    #     else 
+    #         author.save
+    #         session[:skater_id] = skater.id # logging user in
+    #        # redirect '/posts'
+    #     end 
+    # end
+
+
+end 
