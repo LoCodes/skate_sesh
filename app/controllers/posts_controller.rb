3bced54a9b3e1010f036da441075c230db5ae429
Loren Cipriano <loren25cipriano@gmail.com>
2021-02-26 08:04:15 -0800

    added post posts routes and added a new post link in index view

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -5,5 +5,25 @@ class PostsController < ApplicationController
         erb :'posts/index'
     end 
 
+    # get '/posts/new' do 
+    #     erb :'posts/new'
+    # end 
+
+    get '/posts/new' do
+        if logged_in?
+          erb :'posts/new'
+        else
+          redirect to '/login'
+        end
+    end
+
+    post '/posts' do 
+        @post = Post.new(params)
+        @post.skater_id = session[:post_id]
+        @post.save 
+        redirect "/posts/#{@post.id}" 
+
+    end 
+
 
 end  
